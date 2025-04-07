04974ff210107830d6b4a1220310126f931caa65
Gamou09 <martial.gamouza@gmail.com>
2025-04-07 11:24:51 +0100

    Chnaged API calls to use ALPACA

---
+++ config_api_file/secrets.py
@@ -0,0 +1,16 @@
+# should never be pushed to a public space lit github
+# aka include in .gitmore file
+
+
+### ALPACA keys
+
+# Paper Account: PA3TWDFFXTZQ - Basic Momentum
+ALPCA_API_SECRET = 'OwNyrWNebrJpRneMYPbgXwWPl5zki08fI7TEIhlx'
+ALPCA_API_KEY = 'PKTAV1FQCVREC7NI9LG9'
+
+# Other sources keys
+IEX_CLOUD_API_TOKEN = 'Tpk_059b97af715d417d9f49f50b51b1c448'
+INTRINIO_API_TOKEN = 'OjhkMzI3NjgxNDhhMjVlNjU2ZWJkZDQ2ZjEzNTQ5MWVj'
+FMP_API_TOKEN = 'dqK2XwngxkCPv66xOB5PV2wT45ukqAgK'
+EOD_API_TOKEN = '674a93319bd514.55890988'
+
